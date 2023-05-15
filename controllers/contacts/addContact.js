f1577c6bfd6e93cd03ea2c41a1505ad3b19e423e
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2023-05-15 18:24:50 +0300

    Update addContact.js

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,10 +1,10 @@
 const Contact = require('../../models/contact');
 
 const addContact = async (req, res) => {
-  const currentUser = req.user;
-  const contact = await Contact.create(req.body);
-  contact.owner = currentUser;
-  contact.save();
+  const contact = await Contact.create({
+    ...req.body,
+    owner: req.user,
+  });
 
   return res.status(201).json(contact);
 };
