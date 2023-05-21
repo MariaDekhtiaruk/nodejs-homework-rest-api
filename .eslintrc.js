08488006cecf2c5d3c0c2044cb9941f174ee5844
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2023-05-21 18:57:04 +0300

    added unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true, // Add the "jest" environment
   },
   extends: ['standard', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {},
-}
+  rules: {
+    // Allow using expect assertions in Jest tests
+    'jest/expect-expect': 'error',
+  },
+  plugins: ['jest'], // Add the "jest" plugin
+};
