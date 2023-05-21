84724391ae79480e6f0633629d8f4daf74da3ff0
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2023-05-21 18:07:18 +0300

    added Jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const fs = require('fs/promises');
 // const { RequestError } = require('../../helpers');
+const Jimp = require('jimp');
+
+const resizeImage = async (imgPath) => {
+  return Jimp.read(imgPath)
+    .then((img) => {
+      return img
+        .resize(250, 250) // resize
+        .write(imgPath); // save
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
 const updateAvatar = async (req, res) => {
   const { filename } = req.file;
   console.log(req.file);
 
   const tmpPath = path.resolve(__dirname, '../../tmp', filename);
+
+  await resizeImage(tmpPath);
 
   const publicPath = path.resolve(
     __dirname,
